4badbcf93b0f585a6839f4ecb5ea96bea41ff279
khellaf-rachedi <91748769+khellaf-rachedi@users.noreply.github.com>
2021-11-02 18:50:33 +0100

    Add files via upload
    Création d'un fichier index avec des variables str, val, myBool.

---
+++ jour01/job01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$str1="La Plateforme";
+$str2="Vive";
+$str3="!";
+echo "$str2 $str1 $str3 ";
+
+$val=6; 
+echo $val;
+$val +=4;
+echo $val;
+
+$myBool=true;
+echo $myBool; 
+$myBool=false;
+echo $myBool;
+
+?>
